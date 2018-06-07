c1b72b1c438af884ee898e8ec51abdfdc6c1f0b5
David <davidanayadesign@gmail.com>
2018-06-06 20:58:16 -0700

    Update App component
    Reformat + add <main /> wrapper for page content

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React, { Component } from "react";
-import Header from "./components/Header";
-import Icon from "./components/Icon";
-import CopyBlock from "./components/CopyBlock";
-import Footer from "./components/Footer";
-import content from "./content/home.json";
+import React, { Component } from 'react';
+import Header from './components/Header/Header';
+import Paragraph from './components/Paragraph/Paragraph';
+import Footer from './components/Footer/Footer';
+import content from './content/home.json';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header content={content}/>
-        <CopyBlock content={content.copy}/>
-        <Footer />
+        <Header content={content.header} />
+        <main className="App--content">
+          <Paragraph content={content.copy} />
+        </main>
+        <Footer content={content.footer} />
       </div>
     );
   }
